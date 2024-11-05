4f16529edd945881e2ab6eaa88c6baaae641168c
Booker-catch <73317745+Booker-catch@users.noreply.github.com>
2024-11-05 20:10:03 -0300

    agregar consultas

--- proyecto/src/index.js
+++ proyecto/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App2 from '../src/Pages/HitoMotor';
+import App3 from '../src/Pages/Consultas-anteriores';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App2 />
+    <App3 />
   </React.StrictMode>
 );
